58552ba8a0d6167adfb83b87fe2d6128f270ecaa
Jean-Philippe Dupéré <jpdupere@gmail.com>
2021-07-02 01:41:42 -0400

    added axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import AccountBalance from './components/AccountBalance/AccountBalance'
 import React, { Component } from 'react';
+import axios from 'axios';
 import CoinList from './components/CoinList/CoinList';
 import Header from './components/Header/Header';
 import styled from 'styled-components';
@@ -16,15 +17,22 @@ class App extends Component {
     this.state = {
       balance: 10000,
       showBalance: true,
-      coinData: [
-        { name: 'Bitcoin', ticker: 'BTC', price: 31000.00, balance: 0.54 },
-        { name: 'Ethereum', ticker: 'ETH', price: 3000.00, balance: 4 },
-        { name: 'Tether', ticker: 'USDT', price: 1.00, balance: 0 },
-        { name: 'Binance', ticker: 'BNB', price: 300.00, balance: 8 },
-      ]
+      coinData: [],
     }
     this.handleRefresh = this.handleRefresh.bind(this);
     this.handleToggleShowBalance = this.handleToggleShowBalance.bind(this);
+  }
+  async componentDidMount() {
+    const response = await axios.get('https://api.coinpaprika.com/v1/coins');
+    const coinData = response.data.slice(0, 20).map(({ id, name, symbol}) => {
+      return {
+        key: id,
+        name,
+        ticker: symbol,
+      }
+    });
+    this.setState({ coinData });
+    console.log('component did mount');
   }
   handleRefresh(valueChangeTicker) {
     const coinData = this.state.coinData.map(({name, ticker, price, balance}) => {
