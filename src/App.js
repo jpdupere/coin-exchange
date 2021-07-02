9e8316e4347202566262573ef8e392f7a2f3bb3f
Jean-Philippe Dupéré <jpdupere@gmail.com>
2021-07-02 00:13:44 -0400

    merged conflicts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Coin from './components/Coin/Coin'
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="React logo" />
-        <h1>Coin Exchange</h1>
-      </header>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticker</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Coin name="Bitcoin" ticker="BTC" price={30200} />
-          <Coin name="Ethereum" ticker="ETH" price={3500} />
-          <Coin name="Binance" ticker="BNB" price={300} />
-          <Coin name="Tether" ticker="USDT" price={1} />
-        </tbody>
-      </table>
-    </div>
-  );
-=======
 import AccountBalance from './components/AccountBalance/AccountBalance'
 import React, { Component } from 'react';
 import CoinList from './components/CoinList/CoinList';
@@ -87,7 +57,6 @@ class App extends Component {
       </Div>
     );
   }
->>>>>>> 4cb6bf6c9ae278f6e8d263e5083cd4b3acb87132
 }
 
 export default App;
--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,44 +1,4 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD
-import './Coin.css'
-import PropTypes from 'prop-types'
-
-export default class Coin extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            price: this.props.price,
-        }
-    }
-    /*
-    componentDidMount() {
-        const callback = () => {
-            this.setState(oldState => {
-                return { 
-                    ...oldState,
-                    price: oldState.price * (0.995 + Math.random() * 0.01),
-                }
-            })
-        }
-        setInterval(callback, 1000)
-    }
-    */
-    handleClick(evt) {
-        this.setState(oldState => {
-            return { 
-                ...oldState,
-                price: oldState.price * (0.995 + Math.random() * 0.01),
-            }
-        })
-    }
-    render() {
-        return (
-            <tr className="coin-row">
-                <td>{this.props.name}</td>
-                <td>{this.props.ticker}</td>
-                <td>{this.state.price}</td>
-                <td><button onClick={this.handleClick.bind(this)}>Refresh</button></td>
-=======
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
@@ -57,7 +17,6 @@ export default class Coin extends Component {
                 <Td>${this.props.price}</Td>
                 <Td>{balanceText}</Td>
                 <Td><button onClick={() => this.props.handleRefresh(this.props.ticker)}>Refresh</button></Td>
->>>>>>> 4cb6bf6c9ae278f6e8d263e5083cd4b3acb87132
             </tr>
         )
     }
@@ -67,9 +26,5 @@ Coin.propTypes = {
     name: PropTypes.string.isRequired,
     ticker: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-<<<<<<< HEAD
-}
-=======
     handleRefresh: PropTypes.func.isRequired,
 }
->>>>>>> 4cb6bf6c9ae278f6e8d263e5083cd4b3acb87132
